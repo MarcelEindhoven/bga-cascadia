6bd1af31e65835c521a07e492ef10f4711acf5d0
MarcelEindhoven <Marcel.Eindhoven@Hotmail.com>
2024-07-28 19:22:47 +0200

    Creates token in habitat tiles

--- export/modules/javascript/habitat_tiles.js
+++ export/modules/javascript/habitat_tiles.js
@@ -6,6 +6,12 @@ define(['dojo/_base/declare'], (declare) => {
 
         create(tile){
             this.framework.createToken('field', 'tile' + tile.id, 'field' + tile.terrain_types[0]);
+            if (typeof tile.terrain_types[1] != 'undefined') {
+                this.framework.createToken('upper_half', 'upper_half' + tile.id, 'field' + tile.terrain_types[1]);
+            }
+            for (var wildlife_index in tile.supported_wildlife) {
+                this.framework.createToken('field_wildlife', 'field_wildlife' + wildlife_index + tile.id, 'wildlife' + tile.supported_wildlife[wildlife_index]);
+            }
         },
     });
 });
--- test/test_habitat_tiles.js
+++ test/test_habitat_tiles.js
@@ -19,7 +19,7 @@ describe('Habitat tiles', function () {
         };
         it('createToken terrain_types[0]', function () {
             // Arrange
-            tile = {id: 2, terrain_types: [1]};
+            tile = {id: 2, terrain_types: [1], supported_wildlife: [2]};
             // Act
             act_default(tile);
             // Assert
@@ -27,6 +27,39 @@ describe('Habitat tiles', function () {
             assert.equal(framework.createToken.getCall(0).args[0], 'field');
             assert.equal(framework.createToken.getCall(0).args[1], 'tile' + tile.id);
             assert.equal(framework.createToken.getCall(0).args[2], 'field' + tile.terrain_types[0]);
+        });
+        it('createToken supported_wildlife[0]', function () {
+            // Arrange
+            tile = {id: 2, terrain_types: [1], supported_wildlife: [2]};
+            // Act
+            act_default(tile);
+            // Assert
+            assert.equal(framework.createToken.getCall(1).args.length, 3);
+            assert.equal(framework.createToken.getCall(1).args[0], 'field_wildlife');
+            assert.equal(framework.createToken.getCall(1).args[1], 'field_wildlife0' + tile.id);
+            assert.equal(framework.createToken.getCall(1).args[2], 'wildlife' + tile.supported_wildlife[0]);
+        });
+        it('createToken terrain_types[1]', function () {
+            // Arrange
+            tile = {id: 2, terrain_types: [1, 3], supported_wildlife: [2]};
+            // Act
+            act_default(tile);
+            // Assert
+            assert.equal(framework.createToken.getCall(1).args.length, 3);
+            assert.equal(framework.createToken.getCall(1).args[0], 'upper_half');
+            assert.equal(framework.createToken.getCall(1).args[1], 'upper_half' + tile.id);
+            assert.equal(framework.createToken.getCall(1).args[2], 'field' + tile.terrain_types[1]);
+        });
+        it('createToken supported_wildlife[2]', function () {
+            // Arrange
+            tile = {id: 2, terrain_types: [1], supported_wildlife: [2, 4, 5]};
+            // Act
+            act_default(tile);
+            // Assert
+            assert.equal(framework.createToken.getCall(3).args.length, 3);
+            assert.equal(framework.createToken.getCall(3).args[0], 'field_wildlife');
+            assert.equal(framework.createToken.getCall(3).args[1], 'field_wildlife2' + tile.id);
+            assert.equal(framework.createToken.getCall(3).args[2], 'wildlife' + tile.supported_wildlife[2]);
         });
     });
 });
