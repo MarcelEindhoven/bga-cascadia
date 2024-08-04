e2484c6cda7617d58de59c87a59593d549009f7a
MarcelEindhoven <Marcel.Eindhoven@Hotmail.com>
2024-08-04 21:02:29 +0200

    Resize habitat

--- export/modules/javascript/habitat.js
+++ export/modules/javascript/habitat.js
@@ -2,12 +2,49 @@ define(['dojo/_base/declare'], (declare) => {
     return declare('cascadia.habitat', null, {
         constructor(id) {
             this.id = '' + id;
+            this.tiles = [];
+            this.vertical_minimum = 50;
+            this.vertical_maximum = 50;
+            this.horizontal_minimum = 50;
+            this.horizontal_maximum = 50;
         },
         setFramework(framework){this.framework = framework},
         setTileHandler(tile_handler){this.tile_handler = tile_handler;},
 
         place(tile) {
+            this.tiles[tile.id] = tile;
+            this.resize();
             this.tile_handler.move(tile, this.id, x = 0, y = 0);
+        },
+        resize() {
+            vertical_minimum = 50;
+            vertical_maximum = 50;
+            horizontal_minimum = 50;
+            horizontal_maximum = 50;
+            for (id in this.tiles) {
+                horizontal = this.tiles[id].horizontal;
+                vertical = this.tiles[id].vertical;
+                if (horizontal % 2) {vertical = vertical - 0.5;}
+                if (vertical <vertical_minimum) {vertical_minimum = vertical;} else if (vertical >vertical_maximum) {vertical_maximum = vertical;}
+                if (horizontal <horizontal_minimum) {horizontal_minimum = horizontal;} else if (horizontal >horizontal_maximum) {horizontal_maximum = horizontal;}
+            }
+            if ( (vertical_minimum < this.vertical_minimum)|| (vertical_maximum > this.vertical_maximum) || (horizontal_minimum < this.horizontal_minimum) || (horizontal_maximum > this.horizontal_maximum)) {
+                if (horizontal_maximum > this.horizontal_maximum) {
+                    this.horizontal_maximum = horizontal_maximum;
+                }
+                if (horizontal_minimum < this.horizontal_minimum) {
+                    this.horizontal_minimum = horizontal_minimum;                    
+                }
+                if (vertical_maximum > this.vertical_maximum) {
+                    this.vertical_maximum = vertical_maximum;
+                }
+                if (vertical_minimum < this.vertical_minimum) {
+                    this.vertical_minimum = vertical_minimum;
+                }
+                this.framework.resize(this.id, 25 + 24 * (this.horizontal_maximum - this.horizontal_minimum), 25 + 80 *(this.vertical_maximum - this.vertical_minimum));
+            }
+            
+
         },
     });
 });
--- test/test_habitat.js
+++ test/test_habitat.js
@@ -10,6 +10,7 @@ describe('Habitat', function () {
 
         framework = {
             classify: sinon.spy(),
+            resize: sinon.spy(),
         };
         sut.setFramework(framework);
 
@@ -18,12 +19,16 @@ describe('Habitat', function () {
         };
         sut.setTileHandler(tile_handler);
 
-        tile = {id: 2, terrain_types: [1], supported_wildlife: [2], horizontal: 50, vertical: 51};
+        tile = {id: 2, terrain_types: [1], supported_wildlife: [2], horizontal: 50, vertical: 50};
+        other_tile = {id: 22, terrain_types: [1], supported_wildlife: [2], horizontal: 50, vertical: 51};
 
         expected_tile_id = 'tile' + tile.id;
         expected_upper_half_id = 'upper_half' + tile.id;
 
         element = 'test ';
+        minimum_size = 25;
+        vertical_distance = 80;
+        horizontal_distance = 24;
     });
     describe('Place tile', function () {
         function act_default(tile) {
@@ -39,6 +44,84 @@ describe('Habitat', function () {
             assert.equal(tile_handler.move.getCall(0).args[1], '' + player_id);
             assert.equal(tile_handler.move.getCall(0).args[2], 0);
             assert.equal(tile_handler.move.getCall(0).args[3], 0);
+        });
+    });
+    describe('Resize', function () {
+        function act_default(tile) {
+            sut.place(tile);
+        };
+        it('vertical maximum', function () {
+            // Arrange
+            act_default(tile);
+            // Act
+            act_default(other_tile);
+            // Assert
+            assert.equal(framework.resize.getCall(0).args.length, 3);
+            assert.equal(framework.resize.getCall(0).args[0], '' + player_id);
+            assert.equal(framework.resize.getCall(0).args[1], minimum_size);
+            assert.equal(framework.resize.getCall(0).args[2], minimum_size + vertical_distance);
+        });
+        it('vertical minimum', function () {
+            // Arrange
+            act_default(tile);
+            other_tile.vertical = tile.vertical - 1;
+            // Act
+            act_default(other_tile);
+            // Assert
+            assert.equal(framework.resize.getCall(0).args.length, 3);
+            assert.equal(framework.resize.getCall(0).args[0], '' + player_id);
+            assert.equal(framework.resize.getCall(0).args[1], minimum_size);
+            assert.equal(framework.resize.getCall(0).args[2], minimum_size + vertical_distance);
+        });
+        it('Single resize, same other tile', function () {
+            // Arrange
+            act_default(tile);
+            act_default(other_tile);
+            // Act
+            act_default(other_tile);
+            // Assert
+            sinon.assert.callCount(framework.resize, 1);
+        });
+        it('No resize, single tile', function () {
+            // Arrange
+            // Act
+            act_default(tile);
+            // Assert
+            sinon.assert.notCalled(framework.resize);
+        });
+        it('No resize, same tile', function () {
+            // Arrange
+            act_default(tile);
+            // Act
+            act_default(tile);
+            // Assert
+            sinon.assert.notCalled(framework.resize);
+        });
+        it('horizontal minimum', function () {
+            // Arrange
+            act_default(tile);
+            other_tile.horizontal = tile.horizontal - 2;
+            other_tile.vertical = tile.vertical;
+            // Act
+            act_default(other_tile);
+            // Assert
+            assert.equal(framework.resize.getCall(0).args.length, 3);
+            assert.equal(framework.resize.getCall(0).args[0], '' + player_id);
+            assert.equal(framework.resize.getCall(0).args[1], minimum_size + 2 * horizontal_distance);
+            assert.equal(framework.resize.getCall(0).args[2], minimum_size);
+        });
+        it('Even and odd column', function () {
+            // Arrange
+            act_default(tile);
+            other_tile.horizontal = tile.horizontal - 1;
+            other_tile.vertical = tile.vertical;
+            // Act
+            act_default(other_tile);
+            // Assert
+            assert.equal(framework.resize.getCall(0).args.length, 3);
+            assert.equal(framework.resize.getCall(0).args[0], '' + player_id);
+            assert.equal(framework.resize.getCall(0).args[1], minimum_size + horizontal_distance);
+            assert.equal(framework.resize.getCall(0).args[2], minimum_size + vertical_distance / 2);
         });
     });
 });
